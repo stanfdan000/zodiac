b51f9d25e068cda98adcec5da6c836772ce093eb
sirdan <stanford303@gmail.com>
2022-06-06 15:25:01 -0700

    imported the header main footer and rendered list of name and dates and imgs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+import { zodiac } from './data';
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
-      {/* import and use the Header. Main, and Footer components here */}
-      {/* the Header component needs a string passed in as the `name` prop */}
-      {/* the Footer component needs a year passed in as the `year` prop */}
-      {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Header />
+      <Main zodiac={zodiac}/>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+{/* import and use the Header. Main, and Footer components here */}
+{/* the Header component needs a string passed in as the `name` prop */}
+{/* the Footer component needs a year passed in as the `year` prop */}
+{/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,14 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard() {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+
+{/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+export default function ZodiacCard({ name, dates }) {
+  console.log(name, dates);
+  return (
+    <div className='zodiacCard'>
+      <img alt={name} src={process.env.PUBLIC_URL + `images/${name}.png`} />
+      <span className='name'>{name}</span>
+      <span>{dates}</span>
+    </div>
+  );
 }
